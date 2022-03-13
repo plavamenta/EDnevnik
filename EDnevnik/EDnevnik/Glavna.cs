bfcbd80f249820c3b51417b6edf7c2fa293fb82e
plavamenta <96211046+plavamenta@users.noreply.github.com>
2022-03-13 16:54:31 +0100

    Add files via upload

---
+++ EDnevnik/EDnevnik/Glavna.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EDnevnik
+{
+    public partial class Glavna : Form
+    {
+        public Glavna()
+        {
+            InitializeComponent();
+        }
+
+        private void jednaBezFKToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void osobeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Osoba frm_Osoba = new Osoba();
+            frm_Osoba.Show();
+        }
+
+        private void Glavna_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Glavna_Load(object sender, EventArgs e)
+        {
+            string user = Program.user_ime + " " + Program.user_prezime;
+            lbl_korisnik.Text = user;
+        }
+
+        private void smerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Sifarnik frm_sifarnik = new Sifarnik("smer");
+            frm_sifarnik.Show();
+        }
+
+        private void skolskeGodineToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Sifarnik frm_sifarnik = new Sifarnik("skolska_godina");
+            frm_sifarnik.Show();
+
+        }
+
+        private void predmetiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Sifarnik frm_sifarnik = new Sifarnik("predmet");
+            frm_sifarnik.Show();
+        }
+
+        private void osobeToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Sifarnik frm_sifarnik = new Sifarnik("osoba");
+            frm_sifarnik.Show();
+
+        }
+    }
+}
